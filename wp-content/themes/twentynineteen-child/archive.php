bf1f1cc2deae203ad149ccff06dcb37965355aab
Alex Lozitskyi <bineks.test@gmail.com>
2019-04-02 14:31:24 +0300

    first commit 2

--- wp-content/themes/twentynineteen-child/archive.php
+++ wp-content/themes/twentynineteen-child/archive.php
@@ -20,22 +20,18 @@ get_header();
                     <h3 class="text-center">Events</h3>
                     <?php
                     global $paged;
-                    $current_page = get_query_var('paged');
-                    $current_page = max( 1, $current_page );
-                    $offset = ( $current_page - 1 ) * $per_page + 1;
                     $wp_query = new WP_Query(array(
                         'post_type' => 'event',
                         'post_status' => array('publish'),
                         'posts_per_page' => 1,
                         'paged' => $paged,
-                        'offset'=> $offset,
-                        'meta_query' => array(
-                            array(
-                                'key' => 'start_date',
-                                'value' => date('Y-m-d'),
-                                'compare' => '>='
-                            )
-                        )
+//                        'meta_query' => array(
+//                            array(
+//                                'key' => 'start_date',
+//                                'value' => date('Y-m-d'),
+//                                'compare' => '>='
+//                            )
+//                        )
                     )); ?>
                     <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
                         <div>
